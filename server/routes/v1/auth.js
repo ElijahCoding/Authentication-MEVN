b440efe935b1f1dcb48eb8351e6b4d20da915e0e
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-22 21:16:55 +0300

    funcs built

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -1,5 +1,9 @@
 import { Route } from 'express'
+import authController from '../../controllers/v1/auth.controller'
 
 const authRouter = new Router()
+
+authRouter.post('/login', authController.login)
+authRouter.post('/register', authController.register)
 
 export default authRouter
