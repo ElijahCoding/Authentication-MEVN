2a5a3bc72dbe1f2441b3340a3ac9838c9fde4391
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-24 22:49:24 +0300

    webpack client

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
+    mode: process.env.NODE_ENV || 'development',
     entry: './client/index.js',
     output: {
         filename: "app.js",
