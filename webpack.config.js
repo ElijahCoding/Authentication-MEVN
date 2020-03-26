be509a857efcb52d642d8bdf1c641a6609fe057f
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-26 11:37:00 +0300

    vue client set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const path = require('path')
 const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     mode: process.env.NODE_ENV || 'development',
@@ -8,5 +9,24 @@ module.exports = {
     output: {
         filename: "app.js",
         path: path.resolve(__dirname, "server/public")
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.vue$/,
+                use: {
+                    loader: 'vue-loader'
+                }
+            },
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ]
 }
