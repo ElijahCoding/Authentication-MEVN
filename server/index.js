059553c21a54ce93fa1e2c7d0de313b3f90268dc
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-26 10:15:41 +0300

    webpack config

--- server/index.js
+++ server/index.js
@@ -2,10 +2,22 @@ import Express from 'express'
 import Mongoose from 'mongoose'
 import config from '@config'
 import v1Router from '@routes'
+import Webpack from 'webpack'
+import WebpackConfig from '../webpack.config'
+import WebpackDevMiddleware from 'webpack-dev-middleware'
 
 Mongoose.connect(config.databaseUrl, { useNewUrlParser: true })
 
 const app = Express()
+
+const compiler = Webpack(WebpackConfig)
+
+app.use(
+    WebpackDevMiddleware(compiler, {
+        hot: true,
+        publicPath: WebpackConfig.output.publicPath
+    })
+)
 
 app.use(v1Router)
 
