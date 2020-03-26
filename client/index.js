8f33fdf130159a3887cf7113c07beff3c0cecef8
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-26 15:54:15 +0300

    vuex set up, auth

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import router from './routes'
 import Router from 'vue-router'
 import TextInput from '@/client/components/TextInput.vue'
 import Validator from 'vee-validate'
+import store from './store'
 
 Vue.use(Router)
 Vue.use(Validator)
@@ -14,5 +15,6 @@ Vue.component('text-input', TextInput)
 const app = new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(Main)
 })
