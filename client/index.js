e234fc9475c8898691953c64e18d0ce55386f8b0
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-26 15:50:52 +0300

    vue validation

--- client/index.js
+++ client/index.js
@@ -4,8 +4,10 @@ import './styles/main.css'
 import router from './routes'
 import Router from 'vue-router'
 import TextInput from '@/client/components/TextInput.vue'
+import Validator from 'vee-validate'
 
 Vue.use(Router)
+Vue.use(Validator)
 
 Vue.component('text-input', TextInput)
 
