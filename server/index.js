69f6df4fbba268baddab8de37d959e82547be205
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-26 11:28:37 +0300

    webpack , 404 page

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import v1Router from '@routes'
 import Webpack from 'webpack'
 import WebpackConfig from '../webpack.config'
 import WebpackDevMiddleware from 'webpack-dev-middleware'
+import path from 'path'
 
 Mongoose.connect(config.databaseUrl, { useNewUrlParser: true })
 
@@ -20,6 +21,10 @@ app.use(
 )
 
 app.use(v1Router)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(3000, () => {
     console.log('server started.')
